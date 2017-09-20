407668b090953e1c6450e1ac7e6f22f22982c752
srojtas <sascha.rojtas@pitt.edu>
2017-09-20 13:55:45 -0400

    Added Lab 3
    Added Lab 3 to the repo and updated the index file for lab 3.

---
+++ lab3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C/DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="EN" dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Tip of the Day</title>
+</head>
+<body>
+    <h1>Tip of the day</h1>
+    <?php print "<h3>Here's your tip:</h3>";?>
+    <div class="tipArea">
+        <?php readfile("tips.txt");?>
+    </div>
+</body>
+</html>
